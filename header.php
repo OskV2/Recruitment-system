f45b8b2947813ed94464de604500e5db661a00b6
Maciej Buszkiewicz <maciek.buszkiewicz@ideeconcept.pl>
2023-04-05 22:46:24 +0200

    feat: added example header, footer and frontpage files

---
+++ header.php
@@ -0,0 +1,27 @@
+<?php
+    //start of template
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rekrutacja</title>
+
+    <link rel="stylesheet" href="/style.css">
+    <script src="src/js/app.js"></script>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <nav>
+                <ul>
+                    <li><a href="index.php">Strona główna</a></li>
+                    <li><a href="index.php?view=login">Logowanie</a></li>
+                    <li><a href="index.php?view=register">Rejestracja</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
