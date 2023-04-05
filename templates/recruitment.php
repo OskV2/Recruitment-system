b4230ce17882d0b72117edd5f29fed95af468a8a
Maciej Buszkiewicz <maciek.buszkiewicz@ideeconcept.pl>
2023-04-05 22:49:26 +0200

    feat: added basic page files

---
+++ templates/recruitment.php
@@ -0,0 +1,19 @@
+<?php 
+
+    /*** 
+    *
+    *   Template: Strona z ofertami pracy i formularzem rekrutacyjnym
+    *
+    ***/
+
+    include('../header.php') 
+    //write code below
+?>
+
+
+    <p>Rekrutacja</p>
+    
+
+<?php 
+    include('../footer.php') 
+?>
