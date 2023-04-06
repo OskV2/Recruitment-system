0ad91c2266a46aac8a4919c9f05f826592c14f69
Maciej Buszkiewicz <maciek.buszkiewicz@ideeconcept.pl>
2023-04-06 18:09:01 +0200

    feat: added 404 file

---
+++ 404.php
@@ -0,0 +1,8 @@
+<?php include('./header.php') ?>
+
+
+    <!-- write code here -->
+    <h1><?php echo 'Nie znaleziono strony której szukasz' ?></h1>
+    
+
+<?php include('./footer.php') ?>
--- templates/404.php
+++
@@ -1,8 +0,0 @@
-<?php include('../header.php') ?>
-
-
-    <!-- write code here -->
-    <h1><?php __('Nie znaleziono strony której szukasz')?></h1>
-    
-
-<?php include('../footer.php') ?>
