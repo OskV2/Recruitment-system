4cb1dc72f71e4ad82774db4d194ec34f7ff26812
Maciej Buszkiewicz <maciek.buszkiewicz@ideeconcept.pl>
2023-04-15 14:24:05 +0200

    feat: frontpage

--- index.php
+++ index.php
@@ -6,10 +6,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rekrutacja - strona główna</title>
 
-    <link rel="stylesheet" href="/style.css">
-    <script src="src/js/app.js"></script>
+    <link rel="stylesheet" href="./dist/style.css">
+    <script src="dist/app.js"></script>
 </head>
     <body>
-        
+        <div class="row d-flex frontpage">
+
+            <div class="col-6 frontpage__left">
+                <img class="frontpage__left-img" src="./src/img/home1.png" alt="">
+                <p class="frontpage__left-text">SPRAWDŹ PROCES REKRUTACJI</p>
+                <a href="./templates/check.php" class="btn btn-primary stretched-link frontpage__left-button">Sprawdź</a>
+            </div>
+
+            <div class="col-6 frontpage__right">
+                <img class="frontpage__right-img" src="./src/img/home2.png" alt="">
+                <p class="frontpage__right-text">OFERTY PRACY</p>
+                <a href="./templates/recruitment.php" class="btn btn-primary stretched-link frontpage__right-button">Sprawdź</a>
+            </div>
+
+        </div>
     </body>
 </html>
