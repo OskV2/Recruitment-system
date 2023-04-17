b77ee4bcf97d0f0ccf3f166cf7ae0fa4130cbbdb
Melcernik <mateuszmelc@onet.pl>
2023-04-17 18:11:59 +0200

    FET :TEST

--- templates/recruitment.php
+++ templates/recruitment.php
@@ -27,6 +27,7 @@
                             <option value="saab">Saab</option>
                             <option value="opel">Opel</option>
                             <option value="audi">Audi</option>
+                            <option value="audi">SEAT</option>
                         </select>
                     </div>
 
