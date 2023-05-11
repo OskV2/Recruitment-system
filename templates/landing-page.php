d1c211944e10ca9594675c298cbc8d3620cca0ad
Melcernik <mateuszmelc@onet.pl>
2023-05-11 15:29:00 +0200

    feat: working landing-page

--- templates/landing-page.php
+++ templates/landing-page.php
@@ -24,8 +24,10 @@
             $query = "insert into application(Name,Surname,Email,Phone,GitHub_link,Linkdin_Link,Status,Job_Off_Name,Dod_Info) values('$imie','$nazwisko','$email','$telefon','$github_link','$linkedin_link',1,'$job','$Dod_Info')";
     
             if($conn->query($query)){ 
-                // $query = "SELECT TOP 1 App_Id FROM dbo.Application ORDER BY App_Id DESC"; 
-                // $result = $conn->query($query); 
+                $sql = 'SELECT TOP 1 App_Id FROM dbo.Application ORDER BY App_Id DESC';
+                $stmt = $conn->prepare($sql);
+                $stmt->execute();
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
             ?>
                 <main class="landing">
                     <div class="container">
@@ -34,7 +36,7 @@
                                 <div class="landing__content d-flex flex-column justify-content-center">
                                     <h1 class="landing__content-title">Dziękujemy za przesłanie zgłosznia</h1>
                                     <span class="landing__content-description">Twój numer zgłosznia:</span>
-                                    <span class="landing__content-number">1</span>
+                                    <span class="landing__content-number"><?php echo $result['App_Id']; ?></span>
                                     <span class="landing__content-text">Dzięki temu numerowi sprawdzisz status swojego zgłoszenia na stronie "<a href="check.php">Sprawdź aplikację</a>"</span>
                                 </div>
                             </div>
