c847ce11600a9670a8cbb42c7a00a4d00816e4a0
Hedgehog <dopierala.tb@gmail.com>
2023-05-12 22:41:50 +0200

    feat: interview working date

--- templates_sql/insert_admin_notes.php
+++ templates_sql/insert_admin_notes.php
@@ -8,13 +8,16 @@ if(isset($_POST['submit'])){
     $Contract = $_POST['contract'];
     $Salary = $_POST['salary'];
     $Notes = $_POST['notes'];
-    echo $App_ID.$Date.$Contract.$Salary.$Notes;
-    $query = "UPDATE application SET Data_rozmowy = '$Date:00', Umowa = $Contract, Placa = '$Salary PLN/month', Notatki = '$Notes' WHERE App_Id = $App_ID";
+    if (is_null($Date)) {
+        $query = "UPDATE application SET Data_rozmowy = '$Date', Umowa = $Contract, Placa = '$Salary', Notatki = '$Notes' WHERE App_Id = $App_ID";
+    }else{
+        $query = "UPDATE application SET Umowa = $Contract, Placa = '$Salary', Notatki = '$Notes' WHERE App_Id = $App_ID";
+    }
+    // echo $DateSquire;
     echo $query;
-    // $run = mysqli_query($conn, $query) or die(mysqli_error($mysql));
-    // TO_DATE($Date,'YYYY-MM-DD HH24:MI:SS')
+
     $run = $conn->query($query);
-    // $run->bindParam(':app_id', $App_ID);
+
 
     if($run){
         echo "działa";
