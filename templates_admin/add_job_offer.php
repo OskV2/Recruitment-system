3a9aba9ed5934fa3f906758b89dc7e2762f991cb
Maciej Buszkiewicz <maciej.buszkiewicz@o2.pl>
2023-05-12 23:18:56 +0200

    feat: small change in form

--- templates_admin/add_job_offer.php
+++ templates_admin/add_job_offer.php
@@ -7,8 +7,8 @@
                 <form id="formularz" action="../templates_sql/insert_add_job_offer.php" method="post" target="hiddenFrame" onsubmit="setTimeout(function () { window.location.reload(); }, 10)">        
                     <input class="admin__add-input" type="text" id="firma" name="firma" placeholder="Nazwa stanowiska">
                     <input class="admin__add-input" type="text" id="telefon" name="telefon" placeholder="Opis stanowiska">
-                    <input class="admin__add-input" type="text" id="email" name="email" placeholder="Wymagania (wpisz kilka po przecinku zgodnie z przykładem - Javascript, Git, Język Angielski">
-                    <input class="admin__add-input" type="text" id="adres" name="adres" placeholder="Benefity (wpisz kilka po przecinku zgodnie z przykładem - Karta Multisport, Opieka Medyczna">
+                    <input class="admin__add-input" type="text" id="email" name="email" placeholder="Wymagania (wpisz kilka po przecinku zgodnie z przykładem - Javascript, Git, Język Angielski)">
+                    <input class="admin__add-input" type="text" id="adres" name="adres" placeholder="Benefity (wpisz kilka po przecinku zgodnie z przykładem - Karta Multisport, Opieka Medyczna)">
         
                     <button class="btn btn-outline-primary" type="submit" name="submit">Utwórz</button>
                 </form>
