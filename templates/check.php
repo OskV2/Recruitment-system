a4c1e47390f5b0cb4ed9edee74c477c3c5ff9e8e
Melcernik <mateuszmelc@onet.pl>
2023-05-12 21:50:01 +0200

    feat: working check application status

--- templates/check.php
+++ templates/check.php
@@ -1,74 +1,55 @@
 <?php 
 
-    /*** 
-    *
-    *   Template: Strona do sprawdzania statusu swojej rekrutacji
-    *
-    ***/
+/*** 
+*
+*   Template: Strona do sprawdzania statusu swojej rekrutacji
+*
+***/
 
-    include('../header.php');
+include('../header.php');
+include('../templates_sql/db_connect.php');
 
-    if(isset($_POST['submit'])){
-        
-        
-        $App_Id = $_POST['App_Id'];
+if(isset($_POST['App_Id']) && !empty($_POST['App_Id'])){
+  $App_Id = $_POST['App_Id'];
+  $sql = "SELECT Status,Data_rozmowy FROM dbo.Application WHERE App_Id = :App_Id";
+  $stmt = $conn->prepare($sql);
+  $stmt->bindParam(':App_Id', $App_Id);
+  $stmt->execute();
+  $result = $stmt->fetch(PDO::FETCH_ASSOC);
+}
 
-        $sql = 'SELECT Status,Umowa,Data_rozmowy  FROM dbo.Application WHERE App_Id = App_Id = :App_Id';
-        $stmt = $conn->prepare($sql);
-        $stmt->bindParam(':App_Id', $App_Id);
-        $stmt->execute();
-        $result = $stmt->fetch(PDO::FETCH_ASSOC); ?>
+?>
 
-        <main class="check">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12">
-                        <h1 class="check__title">Wpisz numer swojego zgłoszenia</h1>
-                        <form method="post" onsubmit="setTimeout(function () { window.location.reload(); }, 10)">
-                            <input class="check__input" type="number" placeholder="Np. 12345678" maxlength="8" id="App_Id" name="App_Id">
-                            <input class="btn btn-outline-primary ms-auto check__button" type="submit" value="Sprawdź">
-                        </form>
-
-                        <div class="check__output">
-                            <div class="row d-flex">
-                                <div class="col-6 left">
-                                    
-                                    <div class="d-flex">
-                                        <span class="check__output-stats">Status twojego zgłoszenia: <?php echo $result['Status']; ?></span>
-                                    </div>
-
-                                    <div class="check__output-progress">
-
-                                    </div>
-                                    
-                                </div>
-                                <div class="col-6">
-                                    <div class="d-flex flex-column">
-                                        <span class="check__output-date">Data rozmowy: <?php echo $result['Data_rozmowy']; ?></span>
-                                        <span class="check__output-contract">Proponowana umowa: <?php echo $result['Umowa']; ?></span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+<main class="check">
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1 class="check__title">Wpisz numer swojego zgłoszenia</h1>
+        <form method="post" action="">
+          <input class="check__input" type="number" placeholder="Np. 12345678" maxlength="8" id="App_Id" name="App_Id">
+          <input class="btn btn-outline-primary ms-auto check__button" type="submit" name="submit" value="Sprawdź">
+        </form>
+        <div class="check__output">
+          <?php if(isset($result)): ?>
+          <div class="row d-flex">
+            <div class="col-6 left">
+              <div class="d-flex">
+                <span class="check__output-stats">Status twojego zgłoszenia: <span id="status"><?php echo @$result['Status']; ?></span></span>
+              </div>
+              <div class="check__output-progress">
+              </div>
             </div>
-        </main>
-
-    <?php } else { ?>
-        <main class="check">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12">
-                        <h1 class="check__title">Wpisz numer swojego zgłoszenia</h1>
-                        <form method="post" onsubmit="setTimeout(function () { window.location.reload(); }, 10)">
-                            <input class="check__input" type="number" placeholder="Np. 12345678" maxlength="8" id="App_Id" name="App_Id">
-                            <input class="btn btn-outline-primary ms-auto check__button" type="submit" value="Sprawdź">
-                        </form>
-                    </div>
-                </div>
+            <div class="col-6">
+              <div class="d-flex flex-column">
+                <span class="check__output-date">Data rozmowy: <span id="date"><?php echo @$result['Data_rozmowy']; ?></span></span>
+              </div>
             </div>
-        </main>
-    <?php } 
-        include('../footer.php')
-    ?>
+          </div>
+          <?php endif; ?>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
+
+<?php include ('../footer.php'); ?>
