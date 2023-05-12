7c7db4d866e0852af2ccf2f0f441df46e0cf59b4
Maciej Buszkiewicz <maciej.buszkiewicz@o2.pl>
2023-05-12 23:10:45 +0200

    feat: added funtional 404 page

--- 404.php
+++ 404.php
@@ -1,8 +1,14 @@
-<?php include('./header.php') ?>
+<?php include('./header.php'); ?>
 
-
-    <!-- write code here -->
-    <h1><?php echo 'Nie znaleziono strony której szukasz' ?></h1>
-    
+    <div class="page404">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 d-flex justify-content-center align-items-center flex-column">
+                    <h1 class="page404__title">Nie znaleziono strony której szukasz</h1>
+                    <a class="page404__link" href="<?php echo "//" . $_SERVER['HTTP_HOST']; ?>">Powrót na stronę główną</a>
+                </div>
+            </div>
+        </div>
+    </div>
 
 <?php include('./footer.php') ?>
